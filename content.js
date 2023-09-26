b668bfc057769596f4245018642188dde909f297
om2123 <omvakhare150@gmail.com>
2023-09-26 19:27:14 +0530

    followrs and connection count fixed

--- content.js
+++ content.js
@@ -1,15 +1,27 @@
 let userName = document.querySelector(
   "h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words"
 );
-let followers = document.querySelector("span.t-bold");
+let connectionCount;
+let followersCount;
+setTimeout(() => {
+  const about = document.querySelectorAll(`.inline-show-more-text
+  .inline-show-more-text--is-collapsed
+  .inline-show-more-text--is-collapsed-with-line-clamp
+   `)
+   console.log(about);
+  let nums = document.querySelectorAll("span.t-bold");
+  connectionCount = nums[0].textContent;
+  followersCount = nums[1].innerText;
+}, 3000);
+
 let bio = document.querySelector("div.text-body-medium.break-words");
 let locationOfUser = document.querySelector(
   "span.text-body-small.inline.t-black--light.break-words"
 );
 
-let connectionCount = document.querySelectorAll("span .t-bold");
 // let url = connectionCount.baseUrl;
 
+setTimeout(() => {
 // sending data to index.js
 function sendDataToIndex(data) {
   chrome.runtime.sendMessage({
@@ -19,10 +31,11 @@ function sendDataToIndex(data) {
 }
 const userData = {
   name: userName?.textContent || "",
-  followers: followers?.textContent || "not found",
+  followers: followersCount || "not found",
   bio: bio?.textContent || "",
   location: locationOfUser?.textContent || "",
-  connectionCount: connectionCount?.innerHTML || "not found",
+  connectionCount: connectionCount || "not found",
 };
 
-sendDataToIndex(userData);
+  sendDataToIndex(userData);
+}, 4000);
