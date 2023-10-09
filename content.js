823c85fdb636fca86818e1e198214fc9f32a1f51
om2123 <omvakhare150@gmail.com>
2023-10-09 21:40:16 +0530

    step 2:button onclick-will goto linkedin feed

--- content.js
+++ content.js
@@ -1,41 +1,40 @@
-let userName = document.querySelector(
-  "h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words"
-);
-let connectionCount;
-let followersCount;
-setTimeout(() => {
-  const about = document.querySelectorAll(`.inline-show-more-text
-  .inline-show-more-text--is-collapsed
-  .inline-show-more-text--is-collapsed-with-line-clamp
-   `)
-   console.log(about);
-  let nums = document.querySelectorAll("span.t-bold");
-  connectionCount = nums[0].textContent;
-  followersCount = nums[1].innerText;
-}, 3000);
+// let userName = document.querySelector(
+//   "h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words"
+// );
+// let connectionCount;
+// let followersCount;
+// setTimeout(() => {
+//   const about = document.querySelectorAll(`.inline-show-more-text
+//   .inline-show-more-text--is-collapsed
+//   .inline-show-more-text--is-collapsed-with-line-clamp
+//    `)
+//    console.log(about);
+//   let nums = document.querySelectorAll("span.t-bold");
+//   connectionCount = nums[0].textContent;
+//   followersCount = nums[1].innerText;
+// }, 3000);
 
-let bio = document.querySelector("div.text-body-medium.break-words");
-let locationOfUser = document.querySelector(
-  "span.text-body-small.inline.t-black--light.break-words"
-);
+// let bio = document.querySelector("div.text-body-medium.break-words");
+// let locationOfUser = document.querySelector(
+//   "span.text-body-small.inline.t-black--light.break-words"
+// );
+// // let url = connectionCount.baseUrl;
 
-// let url = connectionCount.baseUrl;
+// setTimeout(() => {
+// // sending data to index.js
+// function sendDataToIndex(data) {
+//   chrome.runtime.sendMessage({
+//     type: "dataFromUsers",
+//     data: data,
+//   });
+// }
+// const userData = {
+//   name: userName?.textContent || "",
+//   followers: followersCount || "not found",
+//   bio: bio?.textContent || "",
+//   location: locationOfUser?.textContent || "",
+//   connectionCount: connectionCount || "not found",
+// };
 
-setTimeout(() => {
-// sending data to index.js
-function sendDataToIndex(data) {
-  chrome.runtime.sendMessage({
-    type: "dataFromUsers",
-    data: data,
-  });
-}
-const userData = {
-  name: userName?.textContent || "",
-  followers: followersCount || "not found",
-  bio: bio?.textContent || "",
-  location: locationOfUser?.textContent || "",
-  connectionCount: connectionCount || "not found",
-};
-
-  sendDataToIndex(userData);
-}, 4000);
+//   sendDataToIndex(userData);
+// }, 4000);
--- index.js
+++ index.js
@@ -1,76 +1,33 @@
-// import { getUserData } from "./content";
-
-// making a post request to your backend server.
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "dataFromUsers") {
-    // Save the user data to your backend server.
-    const userData = message.data;
-    console.log(userData);
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json", // Set the content type to JSON
-      },
-      body: JSON.stringify(userData), // Parse userData to JSON before setting it as the request body
-    };
-
-    fetch("http://localhost:3000/users", requestOptions)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Parse the response as JSON
-      })
-      .then((data) => {
-        console.log("User created:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error.message);
-      });
-
-  }
-});
-
-// Define the options for the fetch request
-
-// Make the POST request using the Fetch API
-
 document.addEventListener("DOMContentLoaded", () => {
-  // Get the button that opens the modal
+  // Get references to the input fields and the button
+
+  const likeCountInput = document.getElementById("likeCount");
+  const commentCountInput = document.getElementById("CommentCount");
   const btn = document.getElementById("scrapeButton");
-  // Get the next LinkedIn profile link from the array.
-  const linkedInProfileLinks = [
-    "https://www.linkedin.com/in/rajesh-kr-mehra/",
-    "https://www.linkedin.com/in/kiran-pitambar-bharambe-332336118/",
-    "https://www.linkedin.com/in/vietpham03051999/",
-  ];
 
-  btn.addEventListener("click", async () => {
-    const nextLinkedInProfileLink = linkedInProfileLinks.shift();
+  // Add an event listener to both input fields to check for changes
+  likeCountInput.addEventListener("input", toggleButtonState);
+  commentCountInput.addEventListener("input", toggleButtonState);
 
-    if (!nextLinkedInProfileLink) {
-      return;
+  // Function to toggle the button state based on input values
+  function toggleButtonState() {
+    // Get the values from the input fields
+    const likeCountValue = likeCountInput.value.trim();
+    const commentCountValue = commentCountInput.value.trim();
+
+    // Disable the button if either input field is empty
+    if (likeCountValue === "" || commentCountValue === "") {
+      btn.disabled = true;
+    } else {
+      btn.disabled = false;
     }
-    // Open the LinkedIn profile link in the current tab.
-    chrome.tabs.update({
-      url: nextLinkedInProfileLink,
-    });
-    // Wait for a second to let the LinkedIn profile link load.
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    // Get the active tab.
+  }
+  // Initially, disable the button since both input fields are empty
+  toggleButtonState();
 
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
-    // Send a message to the content script of the active tab
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id, allFrames: true },
-      files: ["content.js"],
+  btn.addEventListener("click", function () {
+    chrome.tabs.update({
+      url: "https://www.linkedin.com/feed/",
     });
   });
-
- 
-  // If there are no more LinkedIn profile links in the array, then exit the function.
 });
